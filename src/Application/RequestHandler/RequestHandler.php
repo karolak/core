5da88ed571e12e77f92f3fe7d7e8f54c32cb602f
Krzysztof Karolak <krzysztof@karolak.it>
2025-02-04 00:32:54 +0100

    Add Request Handler.

---
+++ src/Application/RequestHandler/RequestHandler.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Karolak\Core\Application\RequestHandler;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final class RequestHandler implements RequestHandlerInterface
+{
+    /**
+     * @param RequestHandlerInterface $defaultRequestHandler
+     * @param array<int,MiddlewareInterface> $middlewares
+     */
+    public function __construct(
+        private readonly RequestHandlerInterface $defaultRequestHandler,
+        private array $middlewares = []
+    ) {
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @return ResponseInterface
+     */
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $middleware = array_shift($this->middlewares);
+
+        return $middleware ?
+            $middleware->process($request, new self($this->defaultRequestHandler, $this->middlewares))
+            :
+            $this->defaultRequestHandler->handle($request);
+    }
+}
